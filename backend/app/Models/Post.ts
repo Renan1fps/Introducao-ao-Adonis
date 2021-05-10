6873e4ef5849af639726b9db7d0f96e901f1da05
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-10 16:09:58 -0300

    Definindo model

---
+++ backend/app/Models/Post.ts
@@ -0,0 +1,19 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Post extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public title: string
+
+  @column()//@column({columnName: 'content'}) caso queria mudar o nome da tabela no sistema mas no banco continua a mesma
+  public body: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
